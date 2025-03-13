2fbe46442b7d2d08282727305557725cb8df818e
khahani <mr.khahani@gmail.com>
2025-03-13 22:13:26 +0330

    get current location

--- app/src/main/java/com/khahani/dating/MainActivity.kt
+++ app/src/main/java/com/khahani/dating/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.khahani.dating
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,12 +15,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.khahani.dating.screen.MapScreen
 import com.khahani.dating.ui.theme.LiebeTheme
 
+
+const val LOCATION_REFRESH_TIME = 15000L // 15 seconds to update
+const val LOCATION_REFRESH_DISTANCE = 500f // 500 meters to update
+
 class MainActivity : ComponentActivity() {
+    private val mLocationListener: LocationListener = LocationListener {
+        val location = it
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (getSystemService(LOCATION_SERVICE) as LocationManager).apply {
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE,
+                mLocationListener,
+            )
+        }
         enableEdgeToEdge()
         setContent {
             LiebeTheme {
